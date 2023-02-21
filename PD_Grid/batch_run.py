999de4cda59895b08d8dccf953b9bffb9eec8ba4
ns-beat1213 <ns.beat1213@gmail.com>
2023-02-21 14:54:49 -0600

    add batch run

---
+++ PD_Grid/batch_run.py
@@ -0,0 +1,31 @@
+
+
+import itertools
+
+import mesa
+import numpy as np
+import pandas as pd
+import random
+
+from model import PdGrid
+
+
+
+# parameter lists for each parameter to be tested in batch run
+br_params = {
+    "schedule_type": ["Sequential", "Random","Simultaneous"],
+    "seed": list(range(1,101)),
+    "payoff_type": ['pd', 'sh']
+}
+
+if __name__ == "__main__":
+    data = mesa.batch_run(
+        PdGrid,
+        br_params,
+        max_steps = 50,
+        data_collection_period = 1,
+        iterations = 1,
+    )
+    br_df = pd.DataFrame(data)
+    br_df.to_csv("data/PD_grid.csv")
+
